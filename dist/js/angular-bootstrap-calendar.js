b9e9d55ffc065aeb6122eb097f8b2dcfbd8744ae
Matt Lewis <matt@iwaz.at>
2014-12-04 19:47:28 +0000

    0.5.1 contained a breaking change, so release 0.6.0

--- dist/js/angular-bootstrap-calendar.js
+++ dist/js/angular-bootstrap-calendar.js
@@ -363,6 +363,25 @@ angular.module('mwl.calendar')
 
 'use strict';
 
+
+angular.module('mwl.calendar')
+  .filter('truncateEventTitle', function() {
+
+    return function(string, length, boxHeight) {
+      if (!string) return '';
+
+      //Only truncate if if actually needs truncating
+      if (string.length >= length && string.length / 20 > boxHeight / 30) {
+        return string.substr(0, length) + '...';
+      } else {
+        return string;
+      }
+    };
+
+  });
+
+'use strict';
+
 /**
  * @ngdoc directive
  * @name angularBootstrapCalendarApp.directive:mwlCalendarYear
@@ -668,22 +687,3 @@ angular.module('mwl.calendar')
       }]
     };
   }]);
-
-'use strict';
-
-
-angular.module('mwl.calendar')
-  .filter('truncateEventTitle', function() {
-
-    return function(string, length, boxHeight) {
-      if (!string) return '';
-
-      //Only truncate if if actually needs truncating
-      if (string.length >= length && string.length / 20 > boxHeight / 30) {
-        return string.substr(0, length) + '...';
-      } else {
-        return string;
-      }
-    };
-
-  });
