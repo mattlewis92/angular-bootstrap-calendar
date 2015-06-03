df8be825202dcf0fda5ee0914e174871dd6083cd
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-03 14:12:50 +0100

    Refactor day hours into its own directive

--- src/directives/mwlCalendarDay.js
+++ src/directives/mwlCalendarDay.js
@@ -19,39 +19,24 @@ angular
       controller: function($scope, $timeout, moment, calendarHelper, calendarConfig) {
 
         var vm = this;
-        var dayViewStart, dayViewEnd;
 
         vm.calendarConfig = calendarConfig;
 
-        function updateDays() {
-          dayViewStart = moment($scope.dayViewStart || '00:00', 'HH:mm');
-          dayViewEnd = moment($scope.dayViewEnd || '23:00', 'HH:mm');
-          vm.dayViewSplit = parseInt($scope.dayViewSplit);
-          vm.hourHeight = (60 / $scope.dayViewSplit) * 30;
-          vm.hours = [];
-          var dayCounter = moment(dayViewStart);
-          for (var i = 0; i <= dayViewEnd.diff(dayViewStart, 'hours'); i++) {
-            vm.hours.push({
-              label: dayCounter.format(calendarConfig.dateFormats.hour)
-            });
-            dayCounter.add(1, 'hour');
-          }
-        }
-
-        var originalLocale = moment.locale();
-
         $scope.$on('calendar.refreshView', function() {
+          var dayViewStart = moment($scope.dayViewStart || '00:00', 'HH:mm');
+          var dayViewEnd = moment($scope.dayViewEnd || '23:00', 'HH:mm');
+          var hourHeight = (60 / $scope.dayViewSplit) * 30;
+          vm.dayViewHeight = (dayViewEnd.diff(dayViewStart, 'hours') * hourHeight) + 1;
 
-          if (originalLocale !== moment.locale()) {
-            originalLocale = moment.locale();
-            updateDays();
-          }
-
-          vm.view = calendarHelper.getDayView($scope.events, $scope.currentDay, dayViewStart.hours(), dayViewEnd.hours(), vm.hourHeight);
+          vm.view = calendarHelper.getDayView(
+            $scope.events,
+            $scope.currentDay,
+            dayViewStart.hours(),
+            dayViewEnd.hours(),
+            hourHeight
+          );
 
         });
-
-        updateDays();
 
       },
       controllerAs: 'vm'
