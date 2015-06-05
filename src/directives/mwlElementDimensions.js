c589710a51dc324b56521f0400e5792ba1196d69
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-05 12:29:37 +0100

    Separate directive controllers

--- src/directives/mwlElementDimensions.js
+++ src/directives/mwlElementDimensions.js
@@ -2,16 +2,19 @@
 
 angular
   .module('mwl.calendar')
+  .controller('MwlElementDimensionsCtrl', function($element, $scope, $parse, $attrs) {
+
+    $parse($attrs.mwlElementDimensions).assign($scope, {
+      width: $element[0].offsetWidth,
+      height: $element[0].offsetHeight
+    });
+
+  })
   .directive('mwlElementDimensions', function() {
 
     return {
       restrict: 'A',
-      controller: function($element, $scope, $parse, $attrs) {
-        $parse($attrs.mwlElementDimensions).assign($scope, {
-          width: $element[0].offsetWidth,
-          height: $element[0].offsetHeight
-        });
-      }
+      controller: 'MwlElementDimensionsCtrl'
     };
 
   });
