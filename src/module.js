be66bcf7e914d6d804d3050467e0d6025d48e388
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-21 18:45:35 +0100

    Allow month view templates to be more easily overridden

--- src/module.js
+++ src/module.js
@@ -7,7 +7,12 @@ function requireAll(r) {
   r.keys().forEach(r);
 }
 
-angular.module(MODULE_NAME, []);
+angular //eslint-disable-line angular/ng_module_getter
+  .module(MODULE_NAME, [])
+  .run(function($templateCache) {
+    $templateCache.put('calendarMonthEventsList.html', require('./templates/calendarMonthEventsList.html'));
+    $templateCache.put('calendarMonthDay.html', require('./templates/calendarMonthDay.html'));
+  });
 
 requireAll(require.context('./directives', true, /\.js$/));
 requireAll(require.context('./filters', true, /\.js$/));
