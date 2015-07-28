f8b8821a07afffe4785cfc1d26c7bbc2e36a4bb0
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-28 14:15:54 +0100

    Cleaner exporting of the angular module name

--- src/module.js
+++ src/module.js
@@ -1,24 +1,21 @@
 'use strict';
 
 var angular = require('angular');
-var MODULE_NAME = 'mwl.calendar';
 
 function requireAll(r) {
   r.keys().forEach(r);
 }
 
-angular //eslint-disable-line angular/ng_module_getter
-  .module(MODULE_NAME, [])
+module.exports = angular
+  .module('mwl.calendar', [])
   .constant('calendarUseTemplates', EXCLUDE_TEMPLATES === false)
   .run(function($templateCache, calendarUseTemplates) {
     if (calendarUseTemplates) {
       $templateCache.put('calendarMonthEventsList.html', require('./templates/calendarMonthEventsList.html'));
       $templateCache.put('calendarMonthDay.html', require('./templates/calendarMonthDay.html'));
     }
-  });
+  }).name;
 
 requireAll(require.context('./directives', true, /\.js$/));
 requireAll(require.context('./filters', true, /\.js$/));
 requireAll(require.context('./services', true, /\.js$/));
-
-module.exports = MODULE_NAME;
