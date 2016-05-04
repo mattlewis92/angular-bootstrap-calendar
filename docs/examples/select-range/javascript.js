dd4a3d3c3e8f6ec094c93493e2e861ea7bf862b2
ChipsetSV <chipset89@gmail.com>
2016-05-04 17:50:08 +0300

    feat(functionality): Add function for selecting range on day view.

---
+++ docs/examples/select-range/javascript.js
@@ -0,0 +1,17 @@
+angular
+  .module('mwl.calendar.docs')
+  .controller('SelectRangeCtrl', function(moment) {
+
+    var vm = this;
+
+    vm.events = [];
+    vm.calendarView = 'day';
+    vm.viewDate = moment().startOf('month').toDate();
+    vm.isCellOpen = true;
+
+    vm.rangeSelected = function(startDate, endDate) {
+      vm.firstDateClicked = startDate;
+      vm.lastDateClicked = endDate;
+    };
+
+  });
